edcbc75824fc68f46ed19a5ecc61489e890284c6
Kanishk Tripathi <kanishk.tripathi87@gmail.com>
2015-11-29 14:56:49 -0500

    New scripts

--- filter_tags.py
+++ filter_tags.py
@@ -11,21 +11,8 @@ with open(filename) as data_file:
 tagdata = []
 maxLen = 0
 for proj in data:
- maxLen = max(len(proj['tags']), maxLen)
-tag_header = []
-print maxLen
-i = 1
-while i <= maxLen:
- tag_header.append(i)
- i += 1
-tagdata.append(tag_header)
-for proj in data:
- i = len(proj['tags'])
  tags = proj['tags']
  tags.sort()
- while i < maxLen:
-  tags.append('')
-  i += 1
  tagdata.append(tags)
 with open(fileout, 'wb') as fp:
     a = csv.writer(fp, delimiter=',')
