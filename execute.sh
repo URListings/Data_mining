717b80ad95036ac38ee95b631d9b4700307e7ee1
Kanishk Tripathi <kanishk.tripathi87@gmail.com>
2015-11-30 19:07:51 -0500

    Add comments to shell script

--- execute.sh
+++ execute.sh
@@ -1,33 +1,50 @@
 # Filter scripts
 
+#Cal hacks
 python filter_proj_fields.py proj/calhacks.json class_name,name,tagline,slug,photo,has_video,like_count,comment_count proj_data/calhacks/calhacks.json 1
 
+#Tech crunch filter
 python filter_proj_fields.py proj/techcrunch.json class_name,name,tagline,slug,photo,has_video,like_count,comment_count proj_data/techcrunch/techcrunch.json 1
 
+#Pennapps filter
 python filter_proj_fields.py proj/pennapps.json class_name,name,tagline,slug,photo,has_video,like_count,comment_count proj_data/pennapps/pennapps.json 1
 
+#MHacks filter
 python filter_proj_fields.py proj/mhacks.json class_name,name,tagline,slug,photo,has_video,like_count,comment_count proj_data/mhacks/mhacks.json 1
 
+#HackGT filter
 python filter_proj_fields.py proj/hackgt.json class_name,name,tagline,slug,photo,has_video,like_count,comment_count proj_data/hackgt/hackgt.json 1
 
+#Hack Texas filter
 python filter_proj_fields.py proj/hacktx.json class_name,name,tagline,slug,photo,has_video,like_count,comment_count proj_data/hacktx/hacktx.json 1
 
+#USC filter
 python filter_proj_fields.py proj/hacksc.json class_name,name,tagline,slug,photo,has_video,like_count,comment_count proj_data/hacksc/hacksc.json 1
 
+#Rutgers filter
 python filter_proj_fields.py proj/hackru.json class_name,name,tagline,slug,photo,has_video,like_count,comment_count proj_data/hackru/hackru.json 1
 
+#Hack the north filter
 python filter_proj_fields.py proj/hacknorth.json class_name,name,tagline,slug,photo,has_video,like_count,comment_count proj_data/hacknorth/hacknorth.json 1
 
+#Yale hack filter
 python filter_proj_fields.py proj/yhack.json class_name,name,tagline,slug,photo,has_video,like_count,comment_count proj_data/yhack/yhack.json 1
 
+#Projects file filter
 python filter_proj_fields.py proj/proj.json class_name,tagline,slug,photo,has_video proj_data/proj.json 1
 
 ###############Extract data scripts
+
+#Get tags from the projects
 python filter_tags.py proj_data/proj.json proj_data/tags.csv 1
 
-#Get project tags with true false
+#Get project tags with true false. The last parameter determines the the minimum count of a tag to be extracted
 python filter_tags.py proj_data/proj.json proj_data/tags_true.csv 2 200
+
+#Filter the user data noise
 python filter_proj_fields.py proj/user_data.json avatar_url proj_data/user_data.json 2
+
+#Extract location data of various hackathons
 python filter_user_data.py proj_data/calhacks/calhacks.json proj_data/user_data.jsondict.json proj_data/calhacks/loc.txt
 python filter_user_data.py proj_data/pennapps/pennapps.json proj_data/user_data.jsondict.json proj_data/pennapps/loc.txt
 python filter_user_data.py proj_data/mhacks/mhacks.json proj_data/user_data.jsondict.json proj_data/mhacks/loc.txt
